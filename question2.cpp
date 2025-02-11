59063995bd3e3fab19c378214f85bf8016ffa1b7
arbabarsalan <189278639+arbabarsalan@users.noreply.github.com>
2025-02-12 00:47:44 +0500

    half complete

--- question2.cpp
+++ question2.cpp
@@ -44,25 +44,6 @@ cout<<endl;
 }}
 
 
-
-
-// **********************student detail printing function****************************
-
-
-
-/*
-void print_student_info ( students student[],int size)
-{
-  for(int i=0; i<size; i++)
-{
-cout<< "student "<<i+1<<" id      = "<< student[i].id<<endl;
-cout<< "student "<<i+1<<" name    = "<< student[i].name<<endl;
-cout<< "student "<<i+1<<" age     = "<< student[i].age<<endl;
-cout<< "student "<<i+1<<" gpa     = "<< student[i].gpa<<endl;
-cout<< "student "<<i+1<<" credits = "<< student[i].credits<<endl;
-}}
-*/
-
 void print_student_info(students student[], int size) {
     cout << "\n============================================\n";
     cout << "         STUDENT MANAGEMENT SYSTEM          \n";
@@ -80,6 +61,60 @@ void print_student_info(students student[], int size) {
 }
 
 
+void highest_gpa(students student[], int size) {
+    float highest_gpa = student[0].gpa;
+    int index = 0;
+
+    for (int i = 1; i < size; i++) {
+        if (student[i].gpa > highest_gpa) {
+            highest_gpa = student[i].gpa;
+            index = i;
+        }
+    }
+    cout<<"*************student with highest gpa is**********";
+    cout << "------------------------------------------------------\n";
+    cout << "  ID\tName\tAge\tGPA\tCredits\t\n";
+    cout << "------------------------------------------------------\n";
+    cout << "" << student[index].id <<"\t"<< student[index].name << "\t"<< student[index].age << "\t"<< student[index].gpa <<"\t"<< student[index].credits<<endl;
+
+
+}
+
+
+
+
+void swaping (students student[],int size){
+
+int max_credits=student[0].credits;
+for(int j=0; j<size;j++)
+for (int i = 0; i < size - j - 1; i++) {
+if(student[i].credits<student[i+1].credits)
+{
+int temp_id,temp_age,temp_credits;float temp_gpa; char* temp_name;
+temp_id=student[i].id;
+student[i].id=student[i+1].id;
+student[i+1].id=temp_id;
+
+ temp_name = student[i].name;
+ student[i].name=student[i+1].name;
+ student[i+1].name=temp_name;
+
+temp_age=student[i].age;
+student[i].age=student[i+1].age;
+student[i+1].age=temp_age;
+
+temp_gpa=student[i].gpa;
+student[i].gpa=student[i+1].gpa;
+student[i+1].gpa=temp_gpa;
+
+temp_credits=student[i].credits;
+student[i].credits=student[i+1].credits;
+student[i+1].credits=temp_credits;
+}
+}
+}
+
+
 int main()
 {
 
@@ -90,7 +125,9 @@ cin>>size;
 students *student= new students [size];
 
 studentinfo ( student,size);
-print_student_info(student,size);
+//highest_gpa(student,  size);
+swaping (student, size);
+ print_student_info(student,  size);
 
 
 return 0;
