bc9a2e5252cf4c2e624a8936ad80c12b6f36101d
arbabarsalan <189278639+arbabarsalan@users.noreply.github.com>
2025-02-11 22:48:14 +0500

    Initial Commit

---
+++ question1.cpp
@@ -0,0 +1,74 @@
+
+
+/*
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+
+
+
+    int row =0;
+int colum =0;
+
+cout<<"enter no of rows";
+cin>>row;
+cout<<"enter no of elements you want in array";
+cin>>colum;
+
+int **twoD= new int *[row];
+
+for(int i=0; i<row;i++){
+twoD[i]=new int [colum];
+}
+
+for(int i=0; i<row;i++){
+for(int j=0;j<colum;j++){
+
+cout<<"enter values od 2d array for index  "<<"["<<i<<"]"<<"["<<j<<"] ";
+int value=0;
+cin>>value;
+twoD[i][j]=value;
+
+}
+}
+
+
+ cout << "2D array values are:" << endl;
+ for (int i = 0; i < row; i++) {
+ for (int j = 0; j < colum; j++) {
+            cout << twoD[i][j] << " ";
+        }
+cout << endl;
+    }
+
+     for (int i = 0; i < row; i++) {
+     int rowsum=0;
+ for (int j = 0; j < colum; j++) {
+rowsum+=twoD[i][j];}
+cout<<"row  "<<i <<"  sum ="<<rowsum<<endl;}
+
+
+
+
+     for (int i = 0; i < row; i++) {
+     int colsum=0;
+ for (int j = 0; j < colum; j++) {
+colsum+=twoD[j][i];}
+cout<<"colum "<<i <<"  sum ="<<colsum<<endl;}
+
+
+    for (int i = 0; i < row; i++) {
+        delete[] twoD[i];
+    }
+    delete[] twoD;
+
+return 0;
+
+}
+*/
+
---
+++ question2.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+
+//****************structure**********************
+struct students {
+
+int id;
+char *name;
+int age;
+float gpa;
+int credits;
+
+};
+
+
+
+// *************function for input*********************
+void studentinfo (  students student[], int size){
+
+for (int i=0; i<size ; i++)
+{
+
+cout<<"enter student "<<i+1<<" id        ";cin>>student[i].id;
+int ch;
+cout<<"how many character does your name has?  ";cin>>ch;
+cin.ignore();
+
+
+// ******************dynamoc array for name****************
+student[i].name= new char [ch+1];
+cout<<"enter student "<<i+1<<" name       ";
+cin.getline(student[i].name,ch+1);
+
+
+
+cout<<"enter student  "<<i+1<<" age       ";cin>>student[i].age;
+cout<<"enter student "<<i+1<< " gpa       ";cin>>student[i].gpa;
+cout<<"enter student "<<i+1<< " credits   ";cin>>student[i].credits;
+cout<<endl;
+}}
+
+
+
+
+// **********************student detail printing function****************************
+
+
+
+/*
+void print_student_info ( students student[],int size)
+{
+  for(int i=0; i<size; i++)
+{
+cout<< "student "<<i+1<<" id      = "<< student[i].id<<endl;
+cout<< "student "<<i+1<<" name    = "<< student[i].name<<endl;
+cout<< "student "<<i+1<<" age     = "<< student[i].age<<endl;
+cout<< "student "<<i+1<<" gpa     = "<< student[i].gpa<<endl;
+cout<< "student "<<i+1<<" credits = "<< student[i].credits<<endl;
+}}
+*/
+
+void print_student_info(students student[], int size) {
+    cout << "\n===========================================\n";
+    cout << "   STUDENT MANAGEMENT SYSTEM   \n";
+    cout << "=============================================\n\n";
+
+    cout << "ID      Name      Age      GPA      Credits\n";
+    cout << "----------------------------------------------\n";
+
+    for (int i = 0; i < size; i++) {
+        cout << student[i].id <<   "        "
+             << student[i].name << "        "
+             << student[i].age <<  "        "
+             << student[i].gpa <<  "        "
+             << student[i].credits << endl;
+    }
+}
+
+int main()
+{
+
+cout<<"enter no of students data you want to have ";
+int size=0;
+cin>>size;
+
+students *student= new students [size];
+
+studentinfo ( student,size);
+print_student_info(student,size);
+
+
+return 0;
+
+}
+
+
+
